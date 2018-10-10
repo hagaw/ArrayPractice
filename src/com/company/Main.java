b4f7448908e879d91a18321148a77c0adbb050fb
GBTC406002UR <GBTC406002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-10 11:24:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+      String [] letter ={"w","t","y","h","k"};
+      for(int i=0; i<letter.length;i++){
+
+          if(letter[i] .equals("t")){
+              letter[i]="hello";
+
+          }
+          System.out.println(letter[i]);
+      }
+
+
+
+
+
+    }
+}
